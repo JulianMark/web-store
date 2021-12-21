48f6c1414a01ab4052c1f50b4df3008767da3c62
EXC46189 <julian.markowskyj@claro.com.ar>
2021-12-21 14:43:20 -0300

    Se cambia autoincrement por identity en entity

--- demo-service/src/main/java/com/webstore/demo/entity/Person.java
+++ demo-service/src/main/java/com/webstore/demo/entity/Person.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "ppe_name")
